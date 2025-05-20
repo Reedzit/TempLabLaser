e4eadd5b07fe68eae526a1e0ac12045fcfdc62ee
Xofox <xofox@byu.edu>
2025-05-20 11:01:32 -0600

    Advanced graphing now functional. ReadMe has a detailed explanation of the graphing.

--- src/instrumentInitialize.py
+++ src/instrumentInitialize.py
@@ -103,9 +103,9 @@ class InstrumentInitialize:
             phase = self.lia.query("OUTP? 4")
             return amplitude, phase
         else:
-            print("No lock in amplifier connected")
-            # return random.randint(0, 100), random.randint(0, 360) # For debugging
-            return None, None
+            # print("No lock in amplifier connected")
+            return random.randint(0, 100), random.randint(0, 360) # For debugging
+            # return None, None
 
     def auto_gain(self):
         if self.lia:
@@ -183,7 +183,6 @@ class InstrumentInitialize:
             return None
 
     def update_configuration(self, freq = None, amp = None, offset = None):
-        # TODO: Update channel 2 to have a sine wave.
         if freq:
             print("Using dynamic values")
             if self.fg:
