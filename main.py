af09e5fcdf15c0e2bae0ba553ec090d3933ba910
Xofox <xofox@byu.edu>
2025-05-22 17:20:26 -0600

    Seth, Matplotlib graphing function moved from main thread onto seperate subprocess. Largely done by AI. Moved TODO list onto seperate text file for quality of life. Functional.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import threading
-import multiprocessing as mp
 
 # Add the src directory to the Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
@@ -9,5 +8,4 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 from src.microscopeGUI import MicroscopeGUI
 
 if __name__ == '__main__':
-    mp.set_start_method('spawn')
     MicroscopeGUI()
--- src/gui_tabs/graph_box.py
+++ src/gui_tabs/graph_box.py
@@ -1,134 +1,118 @@
-import multiprocessing
-
+import multiprocessing as mp
+import queue
+import os
+import sys
 from matplotlib import pyplot as plt
-import matplotlib.cm as cm
 import numpy as np
 from PIL import Image
-import queue
-import sys
-import os
 
-# TODO (Done): Change the graph to plot:
-# Phase of LIA (1) vs Frequency of the laser(2) vs Diffusivity(4)
-# Equation thing should output: derivative of: (-(distance between of lasers(user inputed)) * sqrt(pi*freq_laser/diffusivity(what we care about)) = Delta_phase (green vs red))
-# X axis: spot distance*sqrt(pi*freq_laser) Spot distance doesn't really change, so this really just a linear product of frequency
-# Y axis: Phase of LIA (Lock in amplifier) The phase is going to be changing only as a function of the frequency.
-# Find the slope of that graph (Probably the best move here is to estimate it.)
-# 1/slope^2 = Diffusivity (what we care about)
 
-# TODO: Priority: 1 add signal verification to automation. It should be able to tell if the amplitude is high enough. It should read about 200 mV
-
-# TODO: Priority: 2 Allow for sensitivity (Gain) changes. Default is 2mV, you will not go over 50mV. The sensitivity should automatically adjust if the amplitude is maxing out.
-# The options for this are: 1 2 5 10 20 50 100 200 500 (nV through V for all of those values)
-
-# TODO: HIGHEST PRIORITY: Wait for convergence before taking a measurement.
-
-# TODO: Priority: Implement the laser seperation calculation. The software is called Daheng Galaxy Viewer.
-# Information can be found here: https://va-imaging.com/blogs/machine-vision-knowledge-center/daheng-galaxy-viewer-to-program-our-industrial-cameras
-
-# TODO: Priority: 3 Get the CONFIG File to load for the instruments tab.
-
-# TODO: Priority: Find a way to address whether the (green) laser is on or off.
-
-# TODO: Priority: interface the the hexapod into the automation tab. This will be used to move the laser spot around.
-
-# TODO: add uncertainty measurements to the phase and amplitude measurements. Add candlesticks to the graph. 
-# This will be done by taking 10 measurements after stabilization and then taking the standard deviation of those measurements.
-
-# ISSUE: The phase adjustment is not working properly. But we don't need it.
-class GraphBox:
-    def __init__(self, distance):
-        self.laser_distance = distance
-        self.image_queue = queue.LifoQueue(maxsize=2)
-        self.amplitude_data = []
-        self.phase_data = []
-        self.step_data = []
-        self.frequency_data = []
-        self.diffusivity_estimates = []
-
-    def draw_graph(self):
+def plotting_process(data_queue):
+    """Separate process function for plotting"""
+    while True:
         try:
-            plt.clf()  # Clear the current figure
+            data = data_queue.get()
+            if data is None:  # Exit signal
+                break
+
+            frequency_data, phase_data, step_data = data
+
+            plt.clf()
             fig = plt.figure(figsize=(8, 8))
 
-            # Create a scatter plot with color representing step number
-            plt.scatter(self.frequency_data,  # x-axis: phase
-                                  self.phase_data,  # y-axis: amplitude
-                                  c=self.step_data,  # color based on step number
-                                  cmap='viridis',  # color map
-                                  marker='o',  # marker style
-                                  s=100)  # marker size
-            #frequency_delta = self.frequency_data[-1] - self.frequency_data[-2]
-            # plt.bar(self.frequency_data[1:], self.diffusivity_estimates,
-            #                   width=frequency_delta, color='blue', alpha = 1)
+            # Create scatter plot
+            plt.scatter(frequency_data,
+                        phase_data,
+                        c=step_data,
+                        cmap='viridis',
+                        marker='o',
+                        s=100)
 
-            # Add colorbar
+            # Add colorbar and labels
             colorbar = plt.colorbar()
             colorbar.set_label('Step Number')
-
-            # Set labels and title
             plt.title('Phase vs Frequency')
             plt.xlabel('Frequency (Hz)')
             plt.ylabel('Phase (rad)')
             plt.grid(True)
-
 
             # Save the figure
             plots_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'plots')
             if not os.path.exists(plots_dir):
                 os.makedirs(plots_dir)
 
-            image_path = os.path.join(plots_dir, "temp.png")
-            plt.savefig(image_path, dpi=100, bbox_inches='tight')
-            plt.close('all')  # Close all figures to free memory
+            plt.savefig(os.path.join(plots_dir, "temp.png"), dpi=100, bbox_inches='tight')
+            plt.close('all')
 
         except Exception as e:
-            print(f"Error in draw_graph: {e}")
-            import traceback
-            traceback.print_exc()
+            print(f"Error in plotting process: {e}")
+            continue
+
+
+class GraphBox:
+    def __init__(self, distance):
+        self.laser_distance = distance
+        # Regular lists for data storage in main thread
+        self.amplitude_data = []
+        self.phase_data = []
+        self.step_data = []
+        self.frequency_data = []
+        self.diffusivity_estimates = []
+
+        # Create a process-safe queue for plotting
+        self.plot_queue = mp.Queue()
+
+        # Start the plotting process
+        self.plot_process = mp.Process(
+            target=plotting_process,
+            args=(self.plot_queue,),
+            daemon=True
+        )
+        self.plot_process.start()
+
+    def update_graph(self, amplitude, phase, step, frequency):
+        try:
+            # Convert values
+            amplitude = float(amplitude)
+            phase = float(phase)
+            step = int(step)
+
+            # Update data in main thread
+            self.amplitude_data.append(amplitude)
+            self.phase_data.append(phase)
+            self.step_data.append(step)
+            self.frequency_data.append(frequency)
+
+            # Send data to plotting process
+            self.plot_queue.put((
+                self.frequency_data.copy(),
+                self.phase_data.copy(),
+                self.step_data.copy()
+            ))
+
+        except (ValueError, TypeError) as e:
+            print(f"Error updating graph data: {e}")
+            print(f"Values received: amplitude={amplitude}, phase={phase}, step={step}")
 
     def instantaneous_diffusivity(self):
-        # I'm not totally sure where all of these variables are going to come from, but we'll need all of them.
         try:
             delta_frequency = abs(self.frequency_data[-1] - self.frequency_data[-2])
         except IndexError:
             delta_frequency = 1
         delta_x = self.laser_distance * float(np.sqrt(np.pi * delta_frequency))
         try:
-            delta_y = self.phase_data[-1] - self.phase_data[-2]  # This is the same as the change in frequency
+            delta_y = self.phase_data[-1] - self.phase_data[-2]
         except IndexError:
             delta_y = 1
         slope = delta_y / delta_x
-        print(1/slope**2)
-        return (1 / slope ** 2) *1000000
-        # In theory this should be a good instantaneous estimate
-    def update_graph(self, amplitude, phase, step, frequency):
-        try:
-            # Convert string values to float
-            amplitude = float(amplitude)
-            phase = float(phase)
-            step = int(step)
+        print(1 / slope ** 2)
+        return (1 / slope ** 2) * 1000000
 
-            # Append new data
-            self.amplitude_data.append(amplitude)
-            self.phase_data.append(phase)
-            self.step_data.append(step)
-            self.frequency_data.append(frequency)
-            #self.diffusivity_estimates.append(self.instantaneous_diffusivity())
-            print(multiprocessing.active_children())
-            # Draw and save the updated graph
-            self.draw_graph()
-
-        except (ValueError, TypeError) as e:
-            print(f"Error updating graph data: {e}")
-            print(f"Values received: amplitude={amplitude}, phase={phase}, step={step}")
-
-    def queue_image(self):
-        image = Image.open(os.path.join(sys.path[0], "temp.png"), "w")
-        try:
-            self.image_queue.put_nowait(image)
-            return True
-        except queue.Full:
-            print("Queue is full, skipping image")
-            return False
-
+    def __del__(self):
+        # Cleanup: send exit signal to plotting process
+        if hasattr(self, 'plot_queue'):
+            self.plot_queue.put(None)
+        if hasattr(self, 'plot_process'):
+            self.plot_process.join(timeout=1.0)
+            if self.plot_process.is_alive():
+                self.plot_process.terminate()
