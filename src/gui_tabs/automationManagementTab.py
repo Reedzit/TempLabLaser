50ae98c04b04c57c7dcd760e7f5ae8c8150d4de1
xofoxxy <sethleavitt8@gmail.com>
2025-07-17 21:35:35 -0700

    GUI pretty much updated to the optimized version. Now we just need to finalize functionality.

--- src/gui_tabs/automationManagementTab.py
+++ src/gui_tabs/automationManagementTab.py
@@ -1,22 +1,24 @@
 import tkinter as tk
 import tkinter.filedialog
-
+import src.automationManager as automationManager
 
 class AutomationManagerTab:
     def __init__(self, parent, instruments):
         self.parent = parent
         self.instruments = instruments
         self.setup_ui()
+        self.manager = automationManager.AutomationManager(self, instruments, None)
+        self.setup_ui()
 
     def setup_ui(self):
         self.startAutomationButton = tk.Button(self.parent, text="Start Automation", command=self.start_automation)
-        self.startAutomationButton.pack(padx=10, pady=10)
+        self.startAutomationButton.grid(row=0, column=2, padx=10, pady=10)
 
         self.stopAutomationButton = tk.Button(self.parent, text="Stop Automation", command=self.stop_automation)
-        self.stopAutomationButton.pack(padx=10, pady=10)
+        self.stopAutomationButton.grid(row=0, column=3, padx=10, pady=10)
 
     def start_automation(self):
-        self.instruments.start_automation()
+        self.manager.runAutomationCycle()
 
     def stop_automation(self):
-        self.instruments.stop_automation()
+        self.manager.endAutomation()
